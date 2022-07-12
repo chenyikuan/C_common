05004f5e8e6430e931458f89807c54c17ee59758
chenyikuan <512928812@qq.com>
2022-07-12 09:39:48 +0800

    update singleton

--- cyk_singleton.h
+++ cyk_singleton.h
@@ -1,125 +1,30 @@
 #ifndef CYK_SINGLETON_H_
 #define CYK_SINGLETON_H_
-
 #include <iostream>
 #include <list>
 #include <algorithm>
-
-// using namespace std;
-
-class CSingletonBase
-{
-protected:
-	//////////////////////////////////////////////////////
-	//nested class
-	class CInstanceTable : public std::list<CSingletonBase *>
-	{
-	public:
-		CInstanceTable()
-		{
-			m_bIsClearing = false;
-		};
-
-		virtual ~CInstanceTable()
-		{
-			m_bIsClearing = true;
-			for_each(begin(), end(), DeleteInstance);
-		}
-
-	public:
-		static void DeleteInstance(CSingletonBase * pInstance)
-		{
-			delete pInstance;
-		}
-
-	public:
-		bool m_bIsClearing;
-	};
-	//end of nested class
-	///////////////////////////////////////////////////////////
-
-public:
-	CSingletonBase()
-	{
-		CSingletonBase::m_InstanceTbl.push_back(this);
-	}
-
-	virtual ~CSingletonBase()
-	{
-		if(!m_InstanceTbl.m_bIsClearing)
-			m_InstanceTbl.remove(this);
-	}
-
-public:
-	//static member
-	static CInstanceTable m_InstanceTbl;
-};
-
+#include <mutex>
+#include <memory>
 
 template <typename T>
-class cyk_singleton : public CSingletonBase
-{
+class CSingleton {
 public:
-	// double check lock pattern.
-	static T * get_instance()
-	{
-		if(!m_pInstance)
-		{
-			//这里需要使用同步机制！！！
-			if(!m_pInstance) //must check again!
-			{
-				m_pInstance = new T();
-			}
-		}
-
-		return m_pInstance;
-	}
-
+	static T* get_instance() {
+		static std::once_flag onceFlag; // 必须是静态的
+		std::call_once(onceFlag, [&] {m_instance = new T(); }); // 只会调用一次
+		return m_instance;
+	};
+	
 protected:
-	// 所有继承该模板的类，均需设置friend class cyk_singleton<cykTools>;作为友元类，并将自身的构造函数设置为私有
-	cyk_singleton()
-	{
-		//std::cout << "------- Singleton created! -------" << std::endl;
-	}
-
-	virtual ~cyk_singleton()
-	{
-		m_pInstance = NULL;
-		//std::cout << "------- Singleton destroyed! -------" << std::endl;
-	}
+	CSingleton() {}; //私有构造函数，不允许使用者自己生成对象，但是必须要实现
+	CSingleton(const CSingleton& other) = delete;
+	CSingleton& operator = (const CSingleton& other) = delete;
+    ~CSingleton() {delete m_instance;};
 
 private:
-	static T *m_pInstance;
+	static T* m_instance; //静态成员变量 
 };
 
-//must be defined here.
-//if defined in singleton.cpp there will be a link error.
-template<typename T> T * cyk_singleton<T>::m_pInstance = NULL;
-
-/*
-usage:
-	class cyk_test : public cyk_singleton<cyk_test>
-	{
-	protected://or private:  this will prevent programmer creating object directly
-	    friend class cyk_singleton<cyk_test>;
-	    cyk_test(){};
-	public:
-		virtual ~cyk_test() {};
-	    int a;
-	};
-
-OR simple:
-	class cyk_test : public cyk_singleton<cyk_test>
-	{
-	public:
-	    int a;
-	};
-
-Call:
-	cyk_test* x1 = cyk_test::get_instance();
-	cyk_test* x2 = cyk_test::get_instance();
-	x1->a = 10;
-	cout << x2->a << endl;
-*/
+template<typename T> T* CSingleton<T>::m_instance = nullptr;
 
 #endif  //CYK_SINGLETON_H_
