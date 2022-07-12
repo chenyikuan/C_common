7ce62f81da74f01d69332fc53426fcd2a8228031
chenyikuan <512928812@qq.com>
2022-07-12 09:41:38 +0800

    update singleton

--- cyk_singleton.h
+++ cyk_singleton.h
@@ -27,4 +27,30 @@ private:
 
 template<typename T> T* CSingleton<T>::m_instance = nullptr;
 
+/*
+usage:
+    class cyk_test : public CSingleton<cyk_test>
+    {
+    protected://or private:  this will prevent programmer creating object directly
+        friend class CSingleton<cyk_test>;
+        cyk_test(){};
+    public:
+        virtual ~cyk_test() {};
+        int a;
+    };
+
+OR simple:
+    class cyk_test : public CSingleton<cyk_test>
+    {
+    public:
+        int a;
+    };
+
+Call:
+    cyk_test* x1 = cyk_test::get_instance();
+    cyk_test* x2 = cyk_test::get_instance();
+    x1->a = 10;
+    cout << x2->a << endl;
+*/
+
 #endif  //CYK_SINGLETON_H_
