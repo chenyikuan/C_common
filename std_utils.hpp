8e903dd8e751966421ce5912892c94e0bdf9d271
chenyikuan <512928812@qq.com>
2022-11-28 21:47:56 +0800

    update on windows

--- std_utils.hpp
+++ std_utils.hpp
@@ -1,21 +1,8 @@
 #ifndef STD_UTILS_HPP_
 #define STD_UTILS_HPP_
 
+#ifndef _WIN32
 #include <dirent.h>
-#include <string>
-
-
-void strToLower(std::string& str)
-{
-    int i = 0;
-    while (str[i])
-    {
-        str[i] = (std::tolower(str[i]));
-        i++;
-    }
-}
-
-
 std::vector<std::string> getAllFiles(const std::string& path, std::string subfix = "")
 {
     std::vector<std::string> files;
@@ -47,6 +34,52 @@ std::vector<std::string> getAllFiles(const std::string& path, std::string subfix
     }
     closedir(dir);
     return files;
+}
+#else
+#include<io.h>
+#include<stdio.h>
+std::vector<std::string> getAllFiles(std::string path, std::string format)
+{
+    std::vector<std::string> files;
+    long long hFile = 0;//文件句柄  64位下long 改为 intptr_t
+    struct _finddata_t fileinfo;//文件信息     
+    std::string p;    
+    if ((hFile = _findfirst(p.assign(path).append(/*"\\*" + */"*"+format).c_str(), &fileinfo)) != -1)
+        //文件存在    
+    {
+        do    
+        {        
+            if ((fileinfo.attrib & _A_SUBDIR))//判断是否为文件夹            
+            {            
+                if (strcmp(fileinfo.name, ".") != 0 && strcmp(fileinfo.name, "..") != 0)//文件夹名中不含"."和".."            
+                {
+                    // files.push_back(p.assign(path).append("/").append(fileinfo.name)); //保存文件夹名        
+                    // files.append(getAllFiles(p.assign(path).append("/").append(fileinfo.name), format)); //递归遍历文件夹    
+                }        
+            }        
+            else        
+            {        
+                files.push_back(p.assign(path).append("/").append(fileinfo.name));//如果不是文件夹，储存文件名        
+            }        
+        } while (_findnext(hFile, &fileinfo) == 0);        
+        _findclose(hFile);
+    }
+    return files;
+}
+
+#endif
+
+#include <string>
+
+
+void strToLower(std::string& str)
+{
+    int i = 0;
+    while (str[i])
+    {
+        str[i] = (std::tolower(str[i]));
+        i++;
+    }
 }
 
 
